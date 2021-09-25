9a7e13a9235b2c5c0b32a326806d2c2941cb44a6
Alvin Deng <alvinrdeng@gmail.com>
2021-09-25 11:25:23 -0700

    Added models

---
+++ RedTenAngular/DAL/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Player
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(50)]
+        public string Nickname { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ RedTenAngular/DAL/Models/RoundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class RoundPlayer
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int PlayerId { get; set; }
+        [Required]
+        public int RoundId { get; set; }
+        [Required]
+        public int Score { get; set; }
+    }
+}
